cff4b661dad4d69fccae6acf4093ec7ae92f1261
Kyle Lacy <kylelacy@kyle.space>
2026-01-31 14:27:39 -0800

    Update client to connect to WebSocket server

--- weird-web/src/main.ts
+++ weird-web/src/main.ts
@@ -3,3 +3,18 @@ import "./styles/main.css";
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <p>Hello world</p>
 `;
+
+const socket = new WebSocket("http://localhost:2552/ws");
+
+socket.addEventListener("open", (_event) => {
+  console.log("[WebSocket] opened");
+  socket.send("Hello world!");
+});
+
+socket.addEventListener("message", (event) => {
+  console.log("[WebSocket] message", event.data);
+});
+
+socket.addEventListener("error", (event) => {
+  console.warn("WebSocket error:", event);
+});
