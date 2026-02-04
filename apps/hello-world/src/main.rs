5d2f259048d204d4ec004c3c570f4d2c6f63c2f9
Kyle Lacy <kylelacy@kyle.space>
2026-02-03 20:27:00 -0800

    Update `hello-world` to call `show` several times

--- apps/hello-world/src/main.rs
+++ apps/hello-world/src/main.rs
@@ -1,14 +1,28 @@
 use std::{io::Write as _, os::unix::net::UnixStream, path::Path};
 
+const DELAY: std::time::Duration = std::time::Duration::from_millis(500);
+
 fn main() {
     let runtime_dir = std::env::var_os("XDG_RUNTIME_DIR").expect("XDG_RUNTIME_DIR not set");
     let mut weird = UnixStream::connect(Path::new(&runtime_dir).join("weird.sock"))
         .expect("weird.sock not found");
 
+    writeln!(weird, "{}", r#"show ("Starting...")"#).unwrap();
+    std::thread::sleep(DELAY);
+    writeln!(weird, "{}", r#"show ("Running 1/5")"#).unwrap();
+    std::thread::sleep(DELAY);
+    writeln!(weird, "{}", r#"show ("Running 2/5")"#).unwrap();
+    std::thread::sleep(DELAY);
+    writeln!(weird, "{}", r#"show ("Running 3/5")"#).unwrap();
+    std::thread::sleep(DELAY);
+    writeln!(weird, "{}", r#"show ("Running 4/5")"#).unwrap();
+    std::thread::sleep(DELAY);
+    writeln!(weird, "{}", r#"show ("Running 5/5")"#).unwrap();
+    std::thread::sleep(DELAY);
     writeln!(
         weird,
         "{}",
-        r#"show ( "Hello world!" @ProgressBar{total 1, extra 2, children (@Other{} "Text")} )"#
+        r#"show ("Done" @ProgressBar{total 1, extra 2, children (@Other{} "Text")})"#
     )
     .unwrap();
 }
