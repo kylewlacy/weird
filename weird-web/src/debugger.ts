0a90106e2ca5213e1d0b94ce0f851e25f2041e75
Kyle Lacy <kylelacy@kyle.space>
2026-04-05 11:49:43 -0700

    Implement node moving on frontend

--- weird-web/src/debugger.ts
+++ weird-web/src/debugger.ts
@@ -8,6 +8,7 @@ import { ROOT_NODE_ID } from "./world.ts";
 import { ELEMENTS } from "./elements/index.ts";
 import clsx from "clsx";
 import { buttonComponent } from "./elements/Button.ts";
+import unreachable from "ts-unreachable";
 
 export class Debugger {
   #tree: HTMLElement;
@@ -17,7 +18,7 @@ export class Debugger {
   #element: HTMLElement;
 
   constructor() {
-    const rootNode = treeNode({ tag: "World", attributes: {} });
+    const rootNode = treeNode({ tag: "World", attributes: {} }, null);
 
     const closeButton = buttonComponent(
       {
@@ -69,24 +70,166 @@ export class Debugger {
   handleWorldDidChangeEvent(event: WorldDidChangeEvent) {
     for (const removed of event.removed) {
       const removedNode = this.#nodes[removed];
-      if (removedNode) {
+      if (removedNode != null) {
         removedNode.dom.remove();
       } else {
         console.warn("[Debugger] Failed to remove node", { removed });
+      }
+
+      if (removedNode?.parent != null) {
+        const parentNode = this.#nodes[removedNode.parent.id];
+        switch (parentNode?.kind) {
+          case "tree": {
+            parentNode.children.splice(removedNode.parent.index, 1);
+
+            // Update the parent index for each sibling node
+            for (
+              let i = removedNode.parent.index + 1;
+              i < parentNode.children.length;
+              i++
+            ) {
+              const siblingNodeId = parentNode.children[i];
+              const siblingNode =
+                siblingNodeId != null ? this.#nodes[siblingNodeId] : undefined;
+              if (siblingNode?.parent != null) {
+                siblingNode.parent.index = i;
+              }
+            }
+
+            break;
+          }
+          case "leaf": {
+            console.warn("[Debugger] Failed to remove child node from parent", {
+              removed,
+              removedNode,
+              parentNode,
+            });
+            break;
+          }
+          case undefined:
+            // Parent node not found. This can happen if the parent node
+            // was already removed.
+            break;
+          default:
+            return unreachable(parentNode);
+        }
       }
       delete this.#nodes[removed];
     }
 
     for (const inserted of event.inserted) {
       if (inserted.node == null) {
-        throw new Error("TODO: Move node");
+        const node = this.#nodes[inserted.id];
+        if (node == null) {
+          console.warn("[Debugger] Failed to move node", { inserted });
+          continue;
+        }
+
+        const parentNode = this.#nodes[inserted.parentId];
+        if (parentNode?.kind !== "tree") {
+          console.warn(
+            "[Debugger] Failed to get valid parent node when moving",
+            {
+              inserted,
+              parentNode,
+            },
+          );
+          continue;
+        }
+
+        const oldParentNode =
+          node.parent != null ? this.#nodes[node.parent.id] : undefined;
+        if (
+          node.parent == null ||
+          oldParentNode == null ||
+          oldParentNode?.kind !== "tree"
+        ) {
+          console.warn(
+            "[Debugger] Failed to get valid old parent node when moving",
+            { inserted, node, oldParentNode },
+          );
+          continue;
+        }
+        const oldParentNodeId = node.parent.id;
+        const oldParentNodeIndex = node.parent.index;
+
+        if (
+          inserted.parentId === node.parent?.id &&
+          inserted.parentIndex === node.parent.index
+        ) {
+          console.info(
+            "Got move message, but node is moving to it's current position",
+            { inserted },
+          );
+          continue;
+        }
+
+        oldParentNode.children.splice(oldParentNodeIndex, 1);
+        parentNode.children.splice(inserted.parentIndex, 0, inserted.id);
+
+        // Adjust the parent node indices for each child node.
+        // TODO: This can be optimized by limiting the upper bound when
+        // the old and new parent node are the same
+        for (
+          let i = oldParentNodeIndex;
+          i < oldParentNode.children.length;
+          i++
+        ) {
+          const siblingNodeId = oldParentNode.children[i];
+          const siblingNode =
+            siblingNodeId != null ? this.#nodes[siblingNodeId] : undefined;
+          if (siblingNode?.parent != null) {
+            siblingNode.parent.id = oldParentNodeId;
+            siblingNode.parent.index = i;
+          }
+        }
+        if (oldParentNodeId != inserted.parentId) {
+          for (
+            let i = inserted.parentIndex;
+            i < parentNode.children.length;
+            i++
+          ) {
+            const siblingNodeId = parentNode.children[i];
+            const siblingNode =
+              siblingNodeId != null ? this.#nodes[siblingNodeId] : undefined;
+            if (siblingNode?.parent != null) {
+              siblingNode.parent.id = inserted.parentId;
+              siblingNode.parent.index = i;
+            }
+          }
+        }
+
+        const siblingId = parentNode.children[inserted.parentIndex + 1];
+        const sibling = siblingId != null ? this.#nodes[siblingId] : undefined;
+        parentNode.domSlot.insertBefore(node.dom, sibling?.dom ?? null);
       } else {
         const parentNode = this.#nodes[inserted.parentId];
-        const newNode = treeNode(inserted.node);
+        const newNode = treeNode(inserted.node, {
+          id: inserted.parentId,
+          index: inserted.parentIndex,
+        });
         this.#nodes[inserted.id] = newNode;
 
         if (parentNode?.kind === "tree") {
-          parentNode.domSlot.appendChild(newNode.dom);
+          const siblingId = parentNode.children[inserted.parentIndex + 1];
+          const sibling =
+            siblingId != null ? this.#nodes[siblingId] : undefined;
+          parentNode.domSlot.insertBefore(newNode.dom, sibling?.dom ?? null);
+          parentNode.children.splice(inserted.parentIndex, 0, inserted.id);
+
+          // Update the parent index for each sibling node
+          for (
+            let i = inserted.parentIndex + 1;
+            i < parentNode.children.length;
+            i++
+          ) {
+            const siblingNodeId = parentNode.children[i];
+            const siblingNode =
+              siblingNodeId != null ? this.#nodes[siblingNodeId] : undefined;
+            if (siblingNode?.parent != null) {
+              siblingNode.parent.index = i;
+            }
+          }
         } else {
           console.warn("[Debugger] Failed to insert node", { inserted });
         }
@@ -96,10 +239,25 @@ export class Debugger {
 }
 
 type TreeNode =
-  | { kind: "tree"; dom: HTMLElement; domSlot: HTMLElement }
-  | { kind: "leaf"; dom: HTMLElement };
+  | {
+      kind: "leaf";
+      parent: TreeNodeParent | null;
+      dom: HTMLElement;
+    }
+  | {
+      kind: "tree";
+      parent: TreeNodeParent | null;
+      children: NodeId[];
+      dom: HTMLElement;
+      domSlot: HTMLElement;
+    };
 
-function treeNode(node: FlatNode): TreeNode {
+interface TreeNodeParent {
+  id: NodeId;
+  index: number;
+}
+
+function treeNode(node: FlatNode, parent: TreeNodeParent | null): TreeNode {
   // Inspired by: https://iamkate.com/code/tree-views/
   const styleLi = clsx(
     "block relative pl-9 border-l-2 border-gray-400 last:border-transparent before:content-[''] before:block before:absolute before:-top-3 before:-left-0.5 before:w-6.5 before:h-6.25 before:border-gray-400 before:border-b-2 before:border-l-2",
@@ -109,6 +267,7 @@ function treeNode(node: FlatNode): TreeNode {
   if (typeof node === "string") {
     return {
       kind: "leaf",
+      parent,
       dom: h(
         "li",
         {
@@ -143,6 +302,8 @@ function treeNode(node: FlatNode): TreeNode {
     const domSlot = h("ul", { className: styleUl });
     return {
       kind: "tree",
+      parent,
+      children: [],
       dom: h(
         "li",
         {
--- weird-web/src/world.ts
+++ weird-web/src/world.ts
@@ -16,7 +16,7 @@ export class World {
     id: ROOT_NODE_ID,
     tag: "World",
     attributes: {},
-    parentId: undefined,
+    parent: null,
     triggerEvent: (id, event, params) => {
       this.onTriggerEvent?.(id, event, params);
     },
@@ -40,48 +40,46 @@ export class World {
 
       const worldNode = this.nodes[removed];
       if (worldNode == null) {
-        console.warn(
-          "got WorldDidChange event, but node does not exist",
-          removed,
-        );
+        // Node already deleted
         continue;
       }
 
-      switch (worldNode.type) {
-        case "element":
-          worldNode.element.didRemove();
-          break;
-        case "text":
-          break;
-        default:
-          return unreachable(worldNode);
+      if (worldNode.parent == null) {
+        console.warn("tried to remove node with no parent", {
+          worldNode,
+          removed,
+        });
+        continue;
       }
 
-      delete this.nodes[removed];
-
-      const parentNode =
-        worldNode.parentId != null ? this.nodes[worldNode.parentId] : null;
+      const parentNode = this.nodes[worldNode.parent.id];
       switch (parentNode?.type) {
         case "element": {
-          const childIndex = parentNode.children.indexOf(removed);
-          if (childIndex !== -1 && parentNode.element.domSlot != null) {
-            parentNode.children.splice(childIndex, 1);
-            parentNode.element.domSlot.removeChild(worldNode.dom);
-          } else {
-            console.warn("node not found within parent element", {
-              worldNode,
-              parentNode,
-            });
+          parentNode.children.splice(worldNode.parent.index);
+          parentNode.element.domSlot?.removeChild(worldNode.dom);
+
+          // Update the parent index for each sibling node
+          for (
+            let i = worldNode.parent.index;
+            i < parentNode.children.length;
+            i++
+          ) {
+            const siblingNodeId = parentNode.children[i];
+            const siblingNode =
+              siblingNodeId != null ? this.nodes[siblingNodeId] : undefined;
+            if (siblingNode?.parent != null) {
+              siblingNode.parent.index = i;
+            }
           }
+
           break;
         }
         case "text":
           console.warn("invalid parent node type", { worldNode, parentNode });
           break;
-        case null:
         case undefined:
-          // Parent node not found. This is expected since we're removing
-          // nodes outside-in
+          // Parent node not found. This can happen if the parent node
+          // was already removed.
           break;
         default:
           return unreachable(parentNode);
@@ -89,6 +87,7 @@ export class World {
 
       switch (worldNode.type) {
         case "element":
+          worldNode.element.didRemove();
           removeQueue.push(...worldNode.children);
           break;
         case "text":
@@ -96,17 +95,92 @@ export class World {
         default:
           return unreachable(worldNode);
       }
+      delete this.nodes[removed];
     }
 
     for (const inserted of event.inserted) {
       if (inserted.node == null) {
-        throw new Error("TODO: Move node");
+        // Move an existing node
+
+        const worldNode = this.nodes[inserted.id];
+        if (worldNode == null) {
+          throw new Error("tried to move node, but it doesn't exist");
+        }
+
+        const parentNode = this.nodes[inserted.parentId];
+        if (parentNode?.type !== "element") {
+          throw new Error("valid parent not found while moving node");
+        }
+
+        if (parentNode.element.domSlot == null) {
+          throw new Error(
+            `tried to insert node ${inserted.id} but parent ${inserted.parentId} has no DOM slot`,
+          );
+        }
+
+        const oldParentNode =
+          worldNode.parent?.id != null
+            ? this.nodes[worldNode.parent?.id]
+            : undefined;
+        if (worldNode.parent == null || oldParentNode?.type !== "element") {
+          throw new Error("previous valid parent not found while moving node");
+        }
+
+        const oldParentNodeId = worldNode.parent.id;
+        const oldParentNodeIndex = worldNode.parent.index;
+
+        // Adjust the parent node indices for each child node.
+        // TODO: This can be optimized by limiting the upper bound when
+        // the old and new parent node are the same
+        for (
+          let i = oldParentNodeIndex;
+          i < oldParentNode.children.length;
+          i++
+        ) {
+          const siblingNodeId = oldParentNode.children[i];
+          const siblingNode =
+            siblingNodeId != null ? this.nodes[siblingNodeId] : undefined;
+          if (siblingNode?.parent != null) {
+            siblingNode.parent.id = oldParentNodeId;
+            siblingNode.parent.index = i;
+          }
+        }
+        if (oldParentNodeId != inserted.parentId) {
+          for (
+            let i = inserted.parentIndex;
+            i < parentNode.children.length;
+            i++
+          ) {
+            const siblingNodeId = parentNode.children[i];
+            const siblingNode =
+              siblingNodeId != null ? this.nodes[siblingNodeId] : undefined;
+            if (siblingNode?.parent != null) {
+              siblingNode.parent.id = inserted.parentId;
+              siblingNode.parent.index = i;
+            }
+          }
+        }
+
+        oldParentNode.children.splice(oldParentNodeIndex, 1);
+        parentNode.children.splice(inserted.parentIndex, 0, inserted.id);
+
+        const siblingId = parentNode.children[inserted.parentIndex + 1];
+        const sibling = siblingId != null ? this.nodes[siblingId] : undefined;
+        parentNode.element.domSlot.insertBefore(
+          worldNode.dom,
+          sibling?.dom ?? null,
+        );
       } else {
+        // Insert a new node
+
         let worldNode: WorldNode;
         if (typeof inserted.node === "string") {
           worldNode = {
             type: "text",
-            parentId: inserted.parentId,
+            parent: {
+              id: inserted.parentId,
+              index: inserted.parentIndex,
+            },
             text: inserted.node,
             dom: document.createTextNode(inserted.node),
           };
@@ -115,7 +189,10 @@ export class World {
             id: inserted.id,
             tag: inserted.node.tag,
             attributes: inserted.node.attributes,
-            parentId: inserted.parentId,
+            parent: {
+              id: inserted.parentId,
+              index: inserted.parentIndex,
+            },
             triggerEvent: (id: NodeId, event: string, params: unknown) => {
               this.onTriggerEvent?.(id, event, params);
             },
@@ -135,13 +212,22 @@ export class World {
         }
         switch (parentNode?.type) {
           case "element":
-            if (parentNode.element.domSlot == null) {
-              throw new Error(
-                `tried to insert node ${inserted.id} but parent ${inserted.parentId} has no DOM slot`,
+            {
+              if (parentNode.element.domSlot == null) {
+                throw new Error(
+                  `tried to insert node ${inserted.id} but parent ${inserted.parentId} has no DOM slot`,
+                );
+              }
+
+              const siblingId = parentNode.children[inserted.parentIndex + 1];
+              const sibling =
+                siblingId != null ? this.nodes[siblingId] : undefined;
+              parentNode.element.domSlot.insertBefore(
+                worldNode.dom,
+                sibling?.dom ?? null,
               );
+              parentNode.children.splice(inserted.parentIndex, 0, inserted.id);
             }
-            parentNode.children.push(inserted.id);
-            parentNode.element.domSlot.appendChild(worldNode.dom);
             break;
           case "text":
             throw new Error(
@@ -198,23 +284,28 @@ export interface WorldElement {
   tag: string;
   attributes: object;
   children: NodeId[];
-  parentId: NodeId | undefined;
+  parent: WorldNodeParent | null;
   element: WeirdElement;
   dom: Node;
 }
 
 export interface WorldText {
   type: "text";
-  parentId: NodeId | undefined;
+  parent: WorldNodeParent | null;
   text: string;
   dom: Text;
+}
+
+interface WorldNodeParent {
+  id: NodeId;
+  index: number;
 }
 
 interface CreateElementOptions {
   id: NodeId;
   tag: string;
   attributes: object;
-  parentId: NodeId | undefined;
+  parent: WorldNodeParent | null;
   triggerEvent(id: NodeId, event: string, params: unknown): void;
 }
 
@@ -240,7 +331,7 @@ function createElement(opts: CreateElementOptions): WorldElement {
     tag: opts.tag,
     attributes: opts.attributes,
     children: [],
-    parentId: opts.parentId,
+    parent: opts.parent,
     get dom(): Node {
       return this.element.dom;
     },
