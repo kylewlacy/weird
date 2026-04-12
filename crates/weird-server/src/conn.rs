dc610158c1b80a767d0061143317e34a42f4d2b2
Kyle Lacy <kylelacy@kyle.space>
2026-04-11 17:59:56 -0700

    Fix `NextEvent` message blocking window update on disconnection

--- crates/weird-server/src/conn.rs
+++ crates/weird-server/src/conn.rs
@@ -1,6 +1,8 @@
+use std::collections::VecDeque;
+
 use tracing::Instrument;
 use weird_core::{
-    proto::{JsonRpcError, JsonRpcRequest, JsonRpcResponse, Request, Response},
+    proto::{JsonRpcError, JsonRpcRequest, JsonRpcRequestId, JsonRpcResponse, Request, Response},
     world::{ROOT_NODE_ID, World},
 };
 
@@ -16,128 +18,138 @@ pub async fn handle_conn(
     client_out: tokio::sync::mpsc::Sender<JsonRpcResponse<Response>>,
 ) {
     let mut window_node = None;
+    let mut next_event_request_ids = VecDeque::<Option<JsonRpcRequestId>>::new();
 
-    while let Some(request) = client_in.recv().await {
-        tracing::info!("got JSON RPC request: {request:?}");
+    loop {
+        tokio::select! {
+            request = client_in.recv() => {
+                let Some(request) = request else {
+                    break;
+                };
+                tracing::info!("got JSON RPC request: {request:?}");
 
-        match request.body {
-            Request::SyncWorld {} => {
-                let (initial_event, mut events_rx) =
-                    state.world.subscribe_to_world_did_change_events().await;
-                let response = JsonRpcResponse::result(
-                    request.id.clone(),
-                    Response::WorldDidChange(initial_event),
-                );
+                match request.body {
+                    Request::SyncWorld {} => {
+                        let (initial_event, mut events_rx) =
+                            state.world.subscribe_to_world_did_change_events().await;
+                        let response = JsonRpcResponse::result(
+                            request.id.clone(),
+                            Response::WorldDidChange(initial_event),
+                        );
 
-                let out_result = client_out.send(response).await;
-                match out_result {
-                    Ok(()) => {}
-                    Err(error) => {
-                        tracing::warn!("failed to send response: {error}");
-                    }
-                }
+                        let out_result = client_out.send(response).await;
+                        match out_result {
+                            Ok(()) => {}
+                            Err(error) => {
+                                tracing::warn!("failed to send response: {error}");
+                            }
+                        }
 
-                let client_out = client_out.clone();
-                tokio::spawn(
-                    async move {
-                        loop {
-                            let event = events_rx.recv().await;
-                            let event = match event {
-                                Ok(event) => event,
-                                Err(error) => {
-                                    tracing::info!(
-                                        "exiting because WorldChangeEvent subscription failed to receive: {error}"
+                        let client_out = client_out.clone();
+                        tokio::spawn(
+                            async move {
+                                loop {
+                                    let event = events_rx.recv().await;
+                                    let event = match event {
+                                        Ok(event) => event,
+                                        Err(error) => {
+                                            tracing::info!(
+                                                "exiting because WorldChangeEvent subscription failed to receive: {error}"
+                                            );
+                                            break;
+                                        }
+                                    };
+                                    let response = JsonRpcResponse::result(
+                                        request.id.clone(),
+                                        Response::WorldDidChange(event),
                                     );
-                                    break;
-                                }
-                            };
-                            let response = JsonRpcResponse::result(
-                                request.id.clone(),
-                                Response::WorldDidChange(event),
-                            );
 
-                            let out_result = client_out.send(response).await;
-                            match out_result {
-                                Ok(()) => {}
-                                Err(error) => {
-                                    tracing::info!("exiting because WorldDidChange subscription failed to send: {error}");
-                                    break;
+                                    let out_result = client_out.send(response).await;
+                                    match out_result {
+                                        Ok(()) => {}
+                                        Err(error) => {
+                                            tracing::info!("exiting because WorldDidChange subscription failed to send: {error}");
+                                            break;
+                                        }
+                                    }
                                 }
+                            }
+                            .instrument(tracing::info_span!("sync_world")),
+                        );
+                    }
+                    Request::TriggerEvent(trigger_event) => {
+                        let result = state.world.trigger_event(trigger_event).await.map_or_else(
+                            |error| {
+                                Err(JsonRpcError {
+                                    code: 2,
+                                    message: format!("trigger event error: {error:?}"),
+                                    data: serde_json::Value::Null,
+                                })
+                            },
+                            |_| Ok(Response::Empty),
+                        );
+                        let response = JsonRpcResponse::new(request.id, result);
+
+                        let out_result = client_out.send(response).await;
+                        match out_result {
+                            Ok(()) => {}
+                            Err(error) => {
+                                tracing::warn!("failed to send response: {error}");
                             }
                         }
                     }
-                    .instrument(tracing::info_span!("sync_world")),
-                );
-            }
-            Request::TriggerEvent(trigger_event) => {
-                let result = state.world.trigger_event(trigger_event).await.map_or_else(
-                    |error| {
-                        Err(JsonRpcError {
-                            code: 2,
-                            message: format!("trigger event error: {error:?}"),
-                            data: serde_json::Value::Null,
-                        })
-                    },
-                    |_| Ok(Response::Empty),
-                );
-                let response = JsonRpcResponse::new(request.id, result);
-
-                let out_result = client_out.send(response).await;
-                match out_result {
-                    Ok(()) => {}
-                    Err(error) => {
-                        tracing::warn!("failed to send response: {error}");
-                    }
-                }
-            }
-            Request::Render(render) => {
-                let response = if let Some(window_node) = window_node {
-                    let result = state
-                        .world
-                        .set_node_children(window_node, render, conn.id)
-                        .await;
-                    result.map_or_else(
-                        |error| {
-                            Err(JsonRpcError {
-                                code: 1,
-                                message: format!(
-                                    "failed to update node in render request: {error:#?}"
-                                ),
-                                data: serde_json::Value::Null,
-                            })
-                        },
-                        |_| Ok(Response::Empty),
-                    )
-                } else {
-                    window_node = Some(
-                        state
-                            .world
-                            .append_node(
-                                weird_core::world::Element::new("Window")
-                                    .children(render)
-                                    .into(),
-                                ROOT_NODE_ID,
-                                conn.id,
+                    Request::Render(render) => {
+                        let response = if let Some(window_node) = window_node {
+                            let result = state
+                                .world
+                                .set_node_children(window_node, render, conn.id)
+                                .await;
+                            result.map_or_else(
+                                |error| {
+                                    Err(JsonRpcError {
+                                        code: 1,
+                                        message: format!(
+                                            "failed to update node in render request: {error:#?}"
+                                        ),
+                                        data: serde_json::Value::Null,
+                                    })
+                                },
+                                |_| Ok(Response::Empty),
                             )
-                            .await,
-                    );
-                    Ok(Response::Empty)
-                };
-                let response = JsonRpcResponse::new(request.id, response);
+                        } else {
+                            window_node = Some(
+                                state
+                                    .world
+                                    .append_node(
+                                        weird_core::world::Element::new("Window")
+                                            .children(render)
+                                            .into(),
+                                        ROOT_NODE_ID,
+                                        conn.id,
+                                    )
+                                    .await,
+                            );
+                            Ok(Response::Empty)
+                        };
+                        let response = JsonRpcResponse::new(request.id, response);
 
-                let out_result = client_out.send(response).await;
-                match out_result {
-                    Ok(()) => {}
-                    Err(error) => {
-                        tracing::warn!("failed to send response: {error}");
+                        let out_result = client_out.send(response).await;
+                        match out_result {
+                            Ok(()) => {}
+                            Err(error) => {
+                                tracing::warn!("failed to send response: {error}");
+                            }
+                        }
+                    }
+                    Request::NextEvent {} => {
+                        next_event_request_ids.push_back(request.id);
                     }
                 }
             }
-            Request::NextEvent {} => {
-                let event = conn.next_event().await;
+            event = conn.next_event(), if !next_event_request_ids.is_empty() => {
+                let next_event_request_id = next_event_request_ids.pop_front().unwrap();
                 let response = event.map_or_else(|| Response::Empty, Response::Event);
-
-                let response = JsonRpcResponse::result(request.id, response);
+                let response = JsonRpcResponse::result(next_event_request_id, response);
                 let out_result = client_out.send(response).await;
                 match out_result {
                     Ok(()) => {}
