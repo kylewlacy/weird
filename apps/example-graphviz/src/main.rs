9b95f9e9ebc9ded1becb655a2f066c789775a6e3
Kyle Lacy <kylelacy@kyle.space>
2026-04-11 22:47:57 -0700

    Add GraphViz example app

---
+++ apps/example-graphviz/src/main.rs
@@ -0,0 +1,52 @@
+use weird_client::WeirdClient;
+use weird_core::world::Node;
+
+fn main() {
+    let weird = WeirdClient::connect().unwrap();
+
+    let mut engine = "dot".to_string();
+
+    loop {
+        weird.render([
+            Node::text("Example graph"),
+            Node::element("Graphviz")
+                .id("graph")
+                .attr(
+                    "graph",
+                    r#"
+                        digraph {
+                            a -> b
+                        }
+                    "#,
+                )
+                .attr("engine", engine.clone()),
+            Node::text("Engine:"),
+            Node::element("Select")
+                .id("engine")
+                .attr("value", engine.clone())
+                .attr(
+                    "choices",
+                    [
+                        "dot",
+                        "neato",
+                        "fdp",
+                        "sfdp",
+                        "circo",
+                        "twopi",
+                        "osage",
+                        "patchwork",
+                    ],
+                ),
+        ]);
+
+        let Some(event) = weird.next_event().unwrap() else {
+            break;
+        };
+
+        if event.is("engine", "change") {
+            engine = event.param("value").unwrap();
+        } else {
+            // TODO: Log unknown event
+        }
+    }
+}
